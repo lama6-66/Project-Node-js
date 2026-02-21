3549c605bd3229a385acb923fef6be2d964e821b
lama Aljuaid <alotaibilama196@gmail.com>
2026-02-21 14:09:57 +0300

    upload changes

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 import mysql from 'mysql2';
 import { error } from 'console';
-import { insertUser } from './models/mydataSchema.js';
+import fs from "fs";
+import path from 'path';
+import axios from 'axios';
+import { InsertUser } from './models/mydataSchema.js';
 import { ReadData } from './models/mydataSchema.js';
+import {DeleteUser}  from './models/mydataSchema.js';
 
 
 const app=express();
@@ -14,6 +18,10 @@ const __dirname=dirname(fileURLToPath(import.meta.url));
 app.use(express.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static("public"))
+
+app.set("/views", path.join(__dirname, "/views"));
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 
 
@@ -34,35 +42,88 @@ connection.connect((error)=>{
   console.log('connected as id ' + connection.threadId);
 });
 
+// GET Request
+
 app.get("/",(req,res)=>{
 
   ReadData(connection,(err, result)=>{
-    if (err) throw err;
-    console.log(result);
-
-    res.render("home",{
-      username:result[0].userName
-    })
+     if (err) throw err;
+     res.render("common", { body: "home" ,users:result});
   })
-   
-  // res.sendFile('./views/home.html',{root:__dirname});
-  
+
+
+})
+
+app.get("/AddUser",async(req,res)=>{
+  try{
+    const response= await axios.get('https://restcountries.com/v3.1/all?fields=name')
+
+    const countriesList=response.data.map(country => country.name.common ).sort();
+
+     res.render("common", { body: "AddUser" ,isEdit:false,countries:countriesList});
+  }catch(error){
+    console.log(error.message)
+  }
+
+})
+
+app.get("/Edit",(req,res)=>{
+ 
+  res.render("common", { body: "Edit" ,isEdit:true,countries:[]});
 })
  
+app.get("/view/:id",(req,res)=>{
 
-app.post("/post",(req,res)=>{
-  console.log("im here");
-  const name=req.body.name;
+  const userid=req.params.id
 
-  insertUser(connection,name,(err,result)=>{
-    if(err) {
-      console.log(err);
-      throw err;
-    }
-    console.log("1 record inserted");
-    res.send("User added successfully");
+  const sql="SELECT * FROM users WHERE id=?"
+
+  connection.query(sql,[userid],(error,result)=>{
+    if(error) throw error
+   
+  
+  
+  res.render("common", { body: "view",user: result });
+  })
+})
+
+
+
+
+//POST Request
+
+app.post("/AddUser",(req,res)=>{
+
+ const UserData={
+      First_Name:req.body.FirstName,
+      Last_Name:req.body.LastName,
+      Email:req.body.Email,
+      Mobile_Number:req.body.MobileNumber,
+      Age:req.body.Age,
+      Country:req.body.country,
+      gender:req.body.Gender
+ };
+
+ console.log(req.body)
+
+  InsertUser(connection,UserData,function (err, result) {
+    if (err) throw err;
+    res.redirect("/AddUser")
+  });
+  
+});
+
+
+app.get("/deleteUser/:id",(req,res)=>{
+  const id=req.params.id
+
+  DeleteUser(connection,id,(err, result)=> {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+    res.redirect("/")
   });
 })
+
 
 
 app.listen(3000,()=>{
@@ -87,3 +148,33 @@ app.listen(3000,()=>{
 //   .catch((err)=>{
 //     console.log(err);
 //   })
+
+
+
+
+
+
+//  ReadData(connection,(err, result)=>{
+//     if (err) throw err;
+//     console.log(result);
+
+//   //  const homeContent = fs.readFileSync(path.join(__dirname, "views/home.ejs"), "utf8");
+  
+//   })
+   
+  // res.sendFile('./views/home.html',{root:__dirname});
+
+
+// app.post("/post",(req,res)=>{
+//   console.log("im here");
+//   const name=req.body.name;
+
+//   insertUser(connection,name,(err,result)=>{
+//     if(err) {
+//       console.log(err);
+//       throw err;
+//     }
+//     console.log("1 record inserted");
+//     res.send("User added successfully");
+//   });
+// })
--- models/mydataSchema.js
+++ models/mydataSchema.js
@@ -1,7 +1,39 @@
-import mysql from 'mysql2';
+export function InsertUser(connection, UserData, Callback) {
+  let sql =
+    "INSERT INTO users (First_Name, Last_Name,Email,Mobile_Number,Age,Country,gender) VALUES (?,?,?,?,?,?,?)";
+  connection.query(
+    sql,
+    [
+      UserData.First_Name,
+      UserData.Last_Name,
+      UserData.Email,
+      UserData.Mobile_Number,
+      UserData.Age,
+      UserData.Country,
+      UserData.gender,
+    ],
+    Callback,
+  );
+}
+
+
+export function ReadData(connection,Callback){
+    let sql="SELECT * FROM users"
+    connection.query(sql,Callback);
+}
+
+
+
+export function DeleteUser(connection,id,Callback){
+    let sql = "DELETE FROM users WHERE id = ?";
+    connection.query(sql,Callback);
+}
+
+
+
+
+// import mysql from 'mysql2';
 // import { insertUser } from './models/mydataSchema.js';
-
-
 
 // let sql = "INSERT INTO users (userName) VALUES ('Company Inc')";
 //   con.query(sql, function (err, result) {
@@ -9,12 +41,12 @@ import mysql from 'mysql2';
 //     console.log("1 record inserted");
 //   });
 
-export function insertUser(connection,name,callback){
-    let sql = "INSERT INTO users (userName) VALUES (?)";
-    connection.query(sql,[name], callback) 
-}
+// export function insertUser(connection,name,callback){
+//     let sql = "INSERT INTO users (userName) VALUES (?)";
+//     connection.query(sql,[name], callback)
+// }
 
-export function ReadData(connection,callback){
-    let sql = "SELECT userName FROM users";
-    connection.query(sql, callback);
-}
+// export function ReadData(connection,callback){
+//     let sql = "SELECT userName FROM users";
+//     connection.query(sql, callback);
+// }
