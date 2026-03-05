b60622613db6c591620d523a5ef5a521bc4dafd2
lama Aljuaid <alotaibilama196@gmail.com>
2026-03-05 23:02:08 +0300

    Project Completed

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ import axios from "axios";
 import { InsertUser } from "./models/mydataSchema.js";
 import { ReadData } from "./models/mydataSchema.js";
 import { DeleteUser } from "./models/mydataSchema.js";
+import { SearchUser } from "./models/mydataSchema.js";
+import { UpdateUser } from "./models/mydataSchema.js";
+import { getCountriesList } from "./models/mydataSchema.js";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -36,126 +39,105 @@ connection.connect((error) => {
   console.log("connected as id " + connection.threadId);
 });
 
-app.get("/ex",(req,res)=>{
-  res.render("ex");
-})
-
 // GET Request
 
-// app.get("/", (req, res) => {
-//   ReadData(connection, (err, result) => {
-//     if (err) throw err;
-//     res.render("common", { body: "home", users: result });
-//   });
-// });
+app.get("/", (req, res) => {
+  ReadData(connection, (err, result) => {
+    if (err) throw err;
+    res.render("common", { body: "home", users: result });
+  });
+});
 
-// app.get("/AddUser", async (req, res) => {
-//   try {
-//     const response = await axios.get(
-//       "https://restcountries.com/v3.1/all?fields=name",
-//     );
+app.get("/AddUser", async (req, res) => {
+  try {
+    const countries = await getCountriesList();
 
-//     const countriesList = response.data
-//       .map((country) => country.name.common)
-//       .sort();
+    res.render("common", {
+      body: "AddUser",
+      isEdit: false,
+      countries: countries,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-//     res.render("common", {
-//       body: "AddUser",
-//       isEdit: false,
-//       countries: countriesList,
-//     });
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// });
+app.get("/Edit",async (req, res) => {
+  const countries = await getCountriesList();
+  res.render("common", { body: "Edit", isEdit: true, countries: countries });
+});
 
-// app.get("/Edit", (req, res) => {
-//   res.render("common", { body: "Edit", isEdit: true, countries: [] });
-// });
+app.get("/view", (req, res) => {
+  const userid = req.params.id;
 
-// app.get("/view", (req, res) => {
-//   const userid = req.params.id;
+  const sql = "SELECT * FROM users WHERE id=?";
 
-//   const sql = "SELECT * FROM users WHERE id=?";
+  connection.query(sql, [userid], (error, result) => {
+    if (error) throw error;
 
-//   connection.query(sql, [userid], (error, result) => {
-//     if (error) throw error;
+    res.render("common", { body: "view"});
+  });
+});
 
-//     res.render("common", { body: "view"});
-//   });
-// });
+//POST Request
 
-// //POST Request
+app.post("/AddUser", (req, res) => {
+  const UserData = {
+    id: req.body.id,
+    First_Name: req.body.FirstName,
+    Last_Name: req.body.LastName,
+    Email: req.body.Email,
+    Mobile_Number: req.body.MobileNumber,
+    Age: req.body.Age,
+    Country: req.body.country,
+    gender: req.body.Gender,
+  };
 
-// app.post("/AddUser", (req, res) => {
-//   const UserData = {
-//     First_Name: req.body.FirstName,
-//     Last_Name: req.body.LastName,
-//     Email: req.body.Email,
-//     Mobile_Number: req.body.MobileNumber,
-//     Age: req.body.Age,
-//     Country: req.body.country,
-//     gender: req.body.Gender,
-//   };
+  console.log(req.body);
 
-//   console.log(req.body);
+  InsertUser(connection, UserData, function (err, result) {
+    if (err) throw err;
+    res.redirect("/AddUser");
+  });
+});
 
-//   InsertUser(connection, UserData, function (err, result) {
-//     if (err) throw err;
-//     res.redirect("/AddUser");
-//   });
-// });
+app.get("/edit/:id", async (req, res) => {
+  const userid = req.params.id;
+  const countries = await getCountriesList();
+  SearchUser(connection,userid,(err,result)=>{
+    if(err) throw err;
+    res.render("common", { body: "Edit", isEdit: true, countries: countries, user: result[0] });
+})
+});
 
-// app.get("/deleteUser/:id", (req, res) => {
-//   const id = req.params.id;
+app.post("/update/:id", (req, res) => {
+  const userid = req.params.id;
+  const updatedData = {
+    First_Name: req.body.FirstName,
+    Last_Name: req.body.LastName, 
+    Email: req.body.Email,
+    Mobile_Number: req.body.MobileNumber,
+    Age: req.body.Age,
+    Country: req.body.country,
+    gender: req.body.Gender
+  }
+  UpdateUser(connection, userid, updatedData, (err, result) => {
+    if (err) throw err;
+    console.log("Number of records updated: " + result.affectedRows);
+    res.redirect("/");
+  });
+  });
 
-//   DeleteUser(connection, id, (err, result) => {
-//     if (err) throw err;
-//     console.log("Number of records deleted: " + result.affectedRows);
-//     res.redirect("/");
-//   });
-// });
+app.post("/deleteUser/:id", (req, res) => {
+  const id = req.params.id;
+
+  DeleteUser(connection, id, (err, result) => {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, () => {
   console.log("server is running on http://localhost:3000");
 });
-
-// connection.end();
-
-// mongoose.connect(
-//     // "mongodb+srv://lama6-66:ReemSafar66@cluster0.guhotkx.mongodb.net/?appName=Cluster0"
-// )
-//   .then(() => {
-//      console.log("✅ MongoDB Connected");
-//     app.listen(3000,()=>{
-//     console.log('http://localhost:3000/');
-// });
-//   })
-
-//   .catch((err)=>{
-//     console.log(err);
-//   })
-
-//  ReadData(connection,(err, result)=>{
-//     if (err) throw err;
-//     console.log(result);
-
-//   //  const homeContent = fs.readFileSync(path.join(__dirname, "views/home.ejs"), "utf8");
-
-//   })
-
-// res.sendFile('./views/home.html',{root:__dirname});
-
-// app.post("/post",(req,res)=>{
-//   console.log("im here");
-//   const name=req.body.name;
-
-//   insertUser(connection,name,(err,result)=>{
-//     if(err) {
-//       console.log(err);
-//       throw err;
-//     }
-//     console.log("1 record inserted");
-//     res.send("User added successfully");
-//   });
-// })
--- models/mydataSchema.js
+++ models/mydataSchema.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export function InsertUser(connection, UserData, Callback) {
   let sql =
     "INSERT INTO users (First_Name, Last_Name,Email,Mobile_Number,Age,Country,gender) VALUES (?,?,?,?,?,?,?)";
@@ -26,9 +28,27 @@ export function ReadData(connection,Callback){
 
 export function DeleteUser(connection,id,Callback){
     let sql = "DELETE FROM users WHERE id = ?";
-    connection.query(sql,Callback);
+    connection.query(sql,[id],Callback);
 }
 
 
+export function SearchUser(connection,id,Callback){
+  let sql= "SELECT * FROM users WHERE id = ?";
+  connection.query(sql, [id], Callback);
+}
 
 
+export function UpdateUser(connection, id, updatedData, Callback) {
+  let sql = "UPDATE users SET ? WHERE id = ?";
+  connection.query(sql, [updatedData, id], Callback);
+}
+
+export async function getCountriesList() {
+    try {
+        const response = await axios.get("https://restcountries.com/v3.1/all?fields=name");
+        return response.data.map(country => country.name.common).sort();
+    } catch (error) {
+        console.error( error);
+        return []; 
+    }
+}
